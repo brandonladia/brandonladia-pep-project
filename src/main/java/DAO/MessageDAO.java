5d20823f7ffdfb0be515adaa3e6760b549d8af64
Brandon Ladia <ladia93@gmail.com>
2023-09-15 00:17:17 +0000

    skeleton

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -16,13 +16,25 @@ public class MessageDAO {
     }
 
 //retrieve all messages
+    public List<Message> getAllMessages(){
+
+        return null;
+    }
 
 //retrieve a message by ID
+    public Message getMessageByID(){
+
+        return null;
+    }
 
 //delete a message by ID
 
 //update a message by ID
 
 //retrieve all messages from a particular user
+    public List<Message> getAllMessagesFromUser(){
+
+        return null;
+    }
 
 }
