f41892acd8fffd4d9b30e149c1eadc6a7e5f32f3
Brandon Ladia <ladia93@gmail.com>
2023-09-15 00:05:23 +0000

    updated

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -3,6 +3,19 @@ package DAO;
 import java.sql.Connection;
 
 public class MessageDAO {
-    //create connection
+//create connection
     Connection conn;
+
+//create a message
+
+//retrieve all messages
+
+//retrieve a message by ID
+
+//delete a message by ID
+
+//update a message by ID
+
+//retrieve all messages from a particular user
+
 }
