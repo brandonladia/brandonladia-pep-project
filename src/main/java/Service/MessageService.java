53cc2a847d6b2d8dba097bf45f6a6682114acfea
Brandon Ladia <ladia93@gmail.com>
2023-09-18 23:12:03 +0000

    altered code

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -43,10 +43,8 @@ public class MessageService {
     
 //retrieve message by ID
     public Message getMessageByID(int id){
-        if(messageDAO.getMessageByID(id) == null){
-            return null;
-        }
-        return messageDAO.getMessageByID(id);
+        Message getMessage = messageDAO.getMessageByID(id);
+        return getMessage;
     }
 
 //delete message identified by ID
